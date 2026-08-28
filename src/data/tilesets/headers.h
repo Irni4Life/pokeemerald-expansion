2729fc784c1098cd9cb6364df8472f911d74b61e
Irni4Life <adrianrc@hotmail.es>
2026-08-28 16:05:56 +0200

    Created TilesetTest
    Created a test tileset (secondary for now), testing adding palletes and modifying the ingame tilesets

--- src/data/tilesets/headers.h
+++ src/data/tilesets/headers.h
@@ -1541,3 +1541,14 @@ const struct Tileset gTileset_HallOfFame =
 };
 
 #endif // IS_FRLG
+
+const struct Tileset gTileset_Test =
+{
+    .isCompressed = TRUE,
+    .isSecondary = TRUE,
+    .tiles = gTilesetTiles_Test,
+    .palettes = gTilesetPalettes_Test,
+    .metatiles = gMetatiles_Test,
+    .metatileAttributes = gMetatileAttributes_Test,
+    .callback = NULL,
+};
